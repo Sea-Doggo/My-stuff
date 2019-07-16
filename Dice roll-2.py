363c86ba3df4a91e096a2bb8502cc272382ab4f7
Sea-Doggo <52931548+Sea-Doggo@users.noreply.github.com>
2019-07-16 13:15:39 -0600

    Add files via upload

---
+++ Dice roll-2.py
@@ -0,0 +1,14 @@
+import random
+
+def Dicegame(): 
+   
+    print("A dice will be rolled between 1-6 here is the number")
+    randomnum = random.randint(1,6)
+    print("randomnum")
+   
+    
+    
+    Dicegame()
+
+
+    
